8d72ec9ee25668959e80ada5b0aaaa33542239f5
John Oh <chiewoo@gmail.com>
2016-09-22 00:31:16 +0900

    Making Main Index Page

---
+++ MainWebBuild.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from MainHTML import *
+import ast
+import ConfigParser
+config=ConfigParser.ConfigParser()
+config.read("CAGConfig.ini")
+
+BaseDir=config.get('Parameter','Base_Directory')
+stime=int(config.get('Parameter','start_time'))
+etime=int(config.get('Parameter','end_time'))
+srate=config.get('Parameter','sampling_rate')
+stride=config.get('Parameter','time_stride')
+runs=config.get('Parameter','runs')
+datype=config.get('Parameter','datype')
+chlist=ast.literal_eval(config.get('Parameter','chlist'))
+Nseg=int(config.get('Parameter','Nseg'))
+dur=str(etime-stime)
+
+print 'Generating HTML result page...'
+filename=CAGHeader(stime, etime, dur, srate, stride, datype, Nseg, chlist)
+CAGBody(stime, dur, srate, stride, Nseg, datype, chlist, filename)
+CAGFoot(stime, dur, srate, stride, datype, filename, chlist)
+
+print 'All Processes Done.'
+
+
+
+
+
